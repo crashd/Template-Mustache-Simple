bc6bc33bf5b3e837bc9301931e32115205f4a2b1
crashd <marcus.adair@gmail.com>
2011-11-16 08:10:08 +0000

    cleaned up and added pod

--- lib/Template/Mustache/Simple.pm
+++ lib/Template/Mustache/Simple.pm
@@ -11,7 +11,6 @@ our @ISA = qw/Exporter/;
 our %EXPORT_TAGS = ( 'all' => [ qw/render render_file mk_loader/ ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw//;
-our $VERSION = '0.01';
 
 sub new {
 	my $class = shift;
@@ -34,27 +33,30 @@ sub read_file {
 }
 
 sub mk_loader {
-	my ($self, $args) = @_;
-	my $path = $args->{path} || $self->{path};
-	my $ext = $args->{extension} || $self->{extension};
+	my ($self, $path, $extension) = @_;
+	$path		  ||= $self->{path};
+	$extension	  ||= $self->{extension};
 	return sub {
 		my ($name) = @_;
 		-f 
-		return read_file(File::Spec->catfile($path, "$name.$ext"));
+		return read_file(File::Spec->catfile($path, "$name.$extension"));
 	};
 }
 
 sub render {
 	my ($self, $tmpl, $tokens, $args) = @_;
-	$args ||= {};
-	my $cfg = { %$self, %$args };
-	return Template::Mustache->render($tmpl, $tokens, mk_loader($args));
+	my $loader = $self->mk_loader(
+		$args && $args->{path}, 
+		$args && $args->{extension});
+	return Template::Mustache->render($tmpl, $tokens, $loader);
 }
 
 
 sub render_file {
 	my ($self, $name, $tokens, $args) = @_;
-	my $loader = $self->mk_loader($args);
+	my $loader = $self->mk_loader(
+		$args && $args->{path}, 
+		$args && $args->{extension});
 	return Template::Mustache->render($loader->($name), $tokens, $loader);
 }
 
@@ -67,45 +69,62 @@ __END__
 
 =head1 NAME
 
-Template::Mustache::Simple - Perl extension for blah blah blah
+Template::Mustache::Simple - Simple Mustaches for nefariously Perlish disguise
 
 =head1 SYNOPSIS
 
-  use Template::Mustache::Simple;
-  blah blah blah
+  use Template::Mustache::Simple; 
+  
+  \# configure a mustache template parser with optional params
+  \#
+  \# path       : base path where the templates and partials are stored,
+  \#              defaults to current working directory
+  \# 
+  \# extension  : template extension, defaults to "mustache"
+  \#
+  my $mustache = Template::Mustache::Simple->new({
+	path         => "~/htdocs/templates", \# default is current working dir
+	extension    => "stache"              \# default is "mustache"
+  }):
+
+  \# render using string template, $tokens contains template variables
+  my $disguise1 = $mustache->render($template, $hashref, $args);
+
+  \# render from template name, will be prefixed by configured base path
+  \# param, and suffixed with configured extensions, ie:
+  \# 
+  \#    "$path/$name.$extension"
+  \#
+  my $disguise2 = $mustache->render_file($name, $tokens, [ $args ]);
+
+
+  \# with the optional $args hashref, the path and extension can be changed
+  \# for this call only
+  my $disguise3 = $mustache->render_file($name, $tokens, {path => "../tmpl"});
+
+  \# passing an $args hashref works for string template rendering too. a
+  \# path and/or extension passed in here will be used for loading partials
+  my $disguise4 = $mustache->render($template, $tokens, {path => "../tmpl"});
 
 =head1 DESCRIPTION
 
-Stub documentation for Template::Mustache::Simple, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
+A tiny wrapper over Template::Mustache
 
 =head2 EXPORT
 
 None by default.
 
-
-
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+  Template::Mustache
 
 =head1 AUTHOR
 
-A. U. Thor, E<lt>melo@localdomainE<gt>
+crashd
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011 by A. U. Thor
+Copyright (C) 2011 by crashd
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.1 or,
